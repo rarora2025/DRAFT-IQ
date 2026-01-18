9a96be96401d826aa16c841069f5eea2a6407dbd
rarora2025 <rarora2025@gmail.com>
2026-01-17 20:04:07 -0500

    Auto-sync after agent response

---
+++ src/components/PushRegistration.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+/**
+ * Minimal PushRegistration component to satisfy stale build graph references.
+ * This file can be safely deleted once the dev server cache is cleared.
+ */
+export function PushRegistration() {
+  return null
+}
