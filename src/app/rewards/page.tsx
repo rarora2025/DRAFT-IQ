4eefda4ba1c86db1c94c7ee9331909c50c6187f0
rarora2025 <rarora2025@gmail.com>
2026-02-02 00:30:35 -0500

    Auto-sync after agent response

--- src/app/rewards/page.tsx
+++ src/app/rewards/page.tsx
@@ -9,6 +9,7 @@ import { useAuth } from '@/hooks/useAuth'
 import { Button } from '@/components/ui/button'
 import { useTheme } from '@/hooks/useTheme'
 import { toast } from 'sonner'
+import { cn } from '@/lib/utils'
 
 interface RewardMilestone {
   iq: number
